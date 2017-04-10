0df87ae9e96774f7084f9bd78ef4c9f96bd94b42
Colin Darie <colin@darie.eu>
2017-04-10 19:57:54 +0200

    Seeds with 3 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+def open_photo(name)
+  File.open(Rails.root.join("db", "restaurant-images", name))
+end
+
+resto = Restaurant.create!(name: "Aux Petits Oignons",
+  city: "Aix-en-Provence", photo: open_photo("aux-petits-oignons.jpg"))
+
+resto = Restaurant.create!(name: "Via Italia",
+  city: "Aix-en-Provence", photo: open_photo("via-italia.jpg"))
+
+resto = Restaurant.create!(name: "Rowing Club",
+  city: "Marseille", photo: open_photo("rowing-club.jpg"))
