ce8f504c820a5445befc7daa98f40f7961e647bc
Colin Darie <colin@darie.eu>
2017-05-07 17:41:24 +0200

    Gère les cas où un restaurant n’a pas de photo (création par API)

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,10 @@
 module RestaurantsHelper
+  def restaurant_photo_url(restaurant)
+    if restaurant.photo.present?
+      restaurant.photo.path
+    else
+      "https://placehold.it/300x200?text=Photo+du+resto"
+    end
+  end
+
 end
